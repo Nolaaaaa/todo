4b1d9a428600ba532a553c9f49374e7fb6694182
Nolaaaaa <1009198372@qq.com>
2018-07-18 18:46:02 +0800

    input双向绑定

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import Todo from './todo'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  components: { Todo },
+  template: '<Todo/>',
+  created(){
+  },
+  methods: {
+    addTodo(){
+      this.todoList.push({
+        title: this.newTodo,
+        createdAt: new Date(),
+        done: false // 添加一个 done 属性
+      })
+      console.log(this.todoList)
+      this.newTodo = ''
+    },
+  }
 })
+
---
+++ src/todo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <span>TodoList</span>
+    <div class="newTask">
+        <input type="text" v-model="newTodo" @keypress.enter="addTodo()">
+    </div>
+    <ol class="todos">
+        <li v-for="todo in todoList" :key="todo.id">
+          {{ todo.title }}
+        </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: 'todo',
+  data() {
+    return {
+      newTodo: '',
+      todoList: []
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
