20ff9d98c31ff8eb36cd25beebd64390be7405cf
chenwenhao <cwenhao88888@gmail.com>
2018-07-25 17:56:10 +0800

    update style

--- src/components/SignUpOrLogin.vue
+++ src/components/SignUpOrLogin.vue
@@ -1,79 +1,86 @@
 <template>
-    <div id='signUpOrLogin'>
-        <section>
-            <div class="signUp"  v-if="actionType == 'signUp'" >
-                <div class="formRow"> 
-                    用户名<input type="text" v-model="formData.username">
-                </div>
-                <div class="formRow">
-                    密码<input type="password" v-model="formData.password"
-                    @keyup.enter="signUp" >
-                </div>
-                <!-- <div class="formRow">
-                    确认密码<input type="password" v-model="formData.comfirmPassword" >
-                </div> -->
-                <div class="formActions">
-                    <button value="" @click="signUp">注册</button>
-                </div>
-            </div>
-            <div class="login" v-if="actionType == 'login'">
-                <div class="formRow">
-                    用户名<input type="text"  v-model="formData.username">
-                </div>
-                <div class="formRow">
-                    密码<input type="password" v-model="formData.password"
-                    @keyup.enter="login">
-                </div>
-                <div class="formActions">
-                    <button value="" @click="login">登录</button>
-                </div>
-            </div>
-            <div class="button">
-                <el-button @click.native="actionType = 'signUp'" >注册</el-button>
-                <el-button @click.native="actionType = 'login'">登录</el-button>
-            </div>
-        </section>
-    </div>
+  <div id='signUpOrLogin'>
+    <section>
+      <div class="signUp" v-if="actionType == 'signUp'">
+        <div class="formRow">
+          <p>UserName</p>
+          <input type="text" v-model="formData.username" placeholder="输入用户名">
+        </div>
+        <div class="formRow">
+          <p>Password</p>
+          <input type="password" v-model="formData.password" @keyup.enter="signUp" placeholder="密码">
+        </div>
+        <!-- <div class="formRow">
+            确认密码<input type="password" v-model="formData.comfirmPassword" >
+        </div> -->
+        <div class="formActions">
+          <button value="" @click="signUp">注册</button>
+        </div>
+      </div>
+
+      <div class="login" v-if="actionType == 'login'">
+        <div class="formRow">
+          <p>UserName</p>
+          <input type="text" v-model="formData.username" placeholder="输入用户名">
+        </div>
+        <div class="formRow">
+          <p>Password</p>
+          <input type="password" v-model="formData.password"
+                   @keyup.enter="login" placeholder="密码">
+        </div>
+        <div class="formActions">
+          <button value="" @click="login">登录</button>
+        </div>
+      </div>
+
+      <div class="button">
+        <el-button @click="actionType = 'signUp'">注册</el-button>
+        <el-button @click="actionType = 'login'">登录</el-button>
+      </div>
+
+    </section>
+  </div>
 </template>
 
 <script>
-import AV from 'leancloud-storage'
-export default {
+  import AV from 'leancloud-storage'
+
+  export default {
     name: 'SignUpOrLogin',
     data() {
-        return {
-            actionType: 'signUp',
-            formData: {
-                username: '',
-                password: '',
-                comfirmPassword: '',
-            },
-        }
+      return {
+        actionType: 'signUp',
+        formData: {
+          username: '',
+          password: '',
+          comfirmPassword: '',
+        },
+      }
     },
     methods: {
-        signUp() {   //注册 _user
-            this.$emit("signUp",this.formData)
-        },
-        login() {
-            this.$emit("login",this.formData)
-        },
-        
-    }   
-}
+      signUp() {   //注册 _user
+        this.$emit("signUp", this.formData)
+      },
+      login() {
+        this.$emit("login", this.formData)
+      },
+
+    }
+  }
 </script>
 <style lang="scss">
-#signUpOrLogin {
-    background: white;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    section{
-        background: white;
-        padding: 50px;
+  #signUpOrLogin { background: white; display: flex; flex-direction: column;
+    justify-content: center; align-items: center; height: 100vh;
+    section { background: #fff; box-shadow: 0 10px 30px 0 rgba(0, 0, 0, .15); border-radius: 4px;
+      padding: 30px 20px; width: 400px;
+      .formRow { border: 1px solid #c1c1c1; padding: 4px 8px;
+        > p { color: #c1c1c1; }
+        &:first-child { border-bottom: 0; }
+        > input { outline: none; border: 0; }
+      }
+      .formActions { margin: 8px 0; }
     }
-}
+  }
 
 </style>
 
